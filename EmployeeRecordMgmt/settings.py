a6d21ebefbff73c90907aa2cfa8bc4ab7a90a78e
Harsh Gajera <103769647+theharshgajera@users.noreply.github.com>
2024-06-12 22:04:55 +0530

    Update settings.py

--- EmployeeRecordMgmt/settings.py
+++ EmployeeRecordMgmt/settings.py
@@ -121,6 +121,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+ALLOWED_HOSTS = ['194.238.19.164']
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # Default primary key field type
